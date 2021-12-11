cb115c7ded75db13876e9ecb0b92a84c5be37049
PavelKaratkevich <p_korotkevitch@mail.ru>
2021-12-12 00:27:51 +0300

    implement codes 410, 403, 400 and 200

---
+++ instructions.sh
@@ -0,0 +1,27 @@
+create table Lottery (
+id int primary key auto_increment,
+first_name varchar(30),
+last_name varchar(30),
+id_number varchar(10) unique
+)
+DELIMITER $$
+CREATE TRIGGER max_number_of_rows_Lottery
+BEFORE INSERT ON Lottery
+FOR EACH ROW
+BEGIN
+    DECLARE cnt INT;
+
+    SELECT count(*) INTO cnt FROM Lottery;
+
+    IF cnt = 200000 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'All the 10 ticket were sold out';
+    END IF;
+END
+$$
+
+DELIMITER ;
+
+
+
+insert into Lottery (first_name, last_name, id_number)
+values ('Pavel', 'Karatkevich', 'HB2979825')
